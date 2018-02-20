cb4065fb0e15f999531a852dfd762bfdbb3b436b
KsenZ <aksenzov@gmail.com>
2018-02-20 14:54:41 +0700

    Рабочая заготовка класса для работы с БД (reverted from commit ab87f26a0e4328df9ed641dd0db2e5131eff1e07)

--- crawler.py
+++ crawler.py
@@ -1,38 +1,10 @@
-import argparse
-
 from db.db import CrawlerDb
 from parser.parser import Parser
 
 
 class Crawler:
-    def __init__(self):
-        self.db = CrawlerDb()
-        self.parser = Parser()
-
-    def parse_sitemap(self):
-        sites = self.db.get_sites()
-        for site in sites:
-            urls = self.parser.get_all_links_from_sitemap(site["name"])
-            self.db.add_pages(site["id"], urls)
-
-    def parse_pages(self):
-        pass
+    pass
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Crawler for GB internship", usage="%(prog)s [options]")
-    parser.add_argument("-s", "--sitemap", action="store_true", dest="sitemap", help="Parse sitemap")
-    parser.add_argument("-p", "--pages", action="store_true", dest="pages", help="Check unparsed URL")
-
-    args = parser.parse_args()
-
-    if not args.sitemap and not args.pages:
-        parser.error("Set option sitemap or pages")
-
     crawler = Crawler()
-
-    if args.sitemap:
-        crawler.parse_sitemap()
-
-    if args.pages:
-        crawler.parse_pages()
--- db/db.py
+++ db/db.py
@@ -4,25 +4,26 @@ from conf import config
 
 class CrawlerDb:
     def __init__(self):
-        self.cfg = config.CrawlerConfig()
+        pass
 
-    def connect(self):
-        db_host = self.cfg.get_db_host()
-        db_name = self.cfg.get_db_name()
-        db_user = self.cfg.get_db_user()
-        db_pass = self.cfg.get_db_password()
+    @staticmethod
+    def connect():
+        cfg = config.CrawlerConfig()
+        db_host = cfg.get_db_host()
+        db_name = cfg.get_db_name()
+        db_user = cfg.get_db_user()
+        db_pass = cfg.get_db_password()
 
         connection = pymysql.connect(host=db_host,
                                      user=db_user,
                                      password=db_pass,
                                      db=db_name,
-                                     cursorclass=pymysql.cursors.DictCursor,
-                                     autocommit=True)
+                                     cursorclass=pymysql.cursors.DictCursor)
 
         return connection
 
     def get_all_persons(self):
-        sql = "SELECT * FROM `persons`"
+        sql = "SELECT * FROM persons"
         try:
             cursor = self.connect().cursor()
             cursor.execute(sql)
@@ -30,28 +31,3 @@ class CrawlerDb:
             return result
         finally:
             self.connect().close()
-
-    def get_sites(self):
-        sql = "SELECT * FROM `sites`"
-        try:
-            cursor = self.connect().cursor()
-            cursor.execute(sql)
-            result = cursor.fetchall()
-            return result
-        finally:
-            self.connect().close()
-
-    def add_pages(self, site_id, pages):
-        sql = "INSERT INTO `pages` (`url`, `site_id`) VALUES (%s, %s)"
-        n = 0
-        for page in pages:
-            if n < 10:
-                try:
-                    cursor = self.connect().cursor()
-                    cursor.execute(sql, (page, site_id))
-                    self.connect().commit()
-                except pymysql.err.IntegrityError:
-                    pass
-                finally:
-                    self.connect().close()
-                n += 1
